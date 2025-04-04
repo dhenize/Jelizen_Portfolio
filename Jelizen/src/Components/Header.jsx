200d2a7fa00a8de1e6b7f65e11e9177825ecb439
unknown <dhenizelopez@gmail.com>
2025-04-04 21:22:14 +0800

    with cover page na

--- Jelizen/src/Components/Header.jsx
+++ Jelizen/src/Components/Header.jsx
@@ -3,38 +3,38 @@ import logo from '../assets/pics/logo.png';
 
 function Header() {
   return (
-    <header className = "flex items-row justify-between px-4 py-3 sm:px-6 md:px-8 lg:px-10 bg-[#1A1714] shadow-xl/20">
+    <header className = "flex items-row justify-between bg-[#1A1714] shadow-xl/20 px-4 py-3 sm:px-5 md:px-8 lg:px-10">
         <div className="flex items-center flex-shrink-0">
           <img className = "h-9 w-auto" src = {logo}/>
         </div>
         
         <nav className = "flex items-center sm:space-x-2 md:space-x-3 lg:space-x-4">
           <Link to="/">
-            <button type="button" className="h-12 w-20 bg-[rgba(61,79,61,0.7)] text-[#33FFC2] border border-transparent hover:border-[#33FFC2]">
+            <button type="button" className="h-12 w-20 text-[15px] text-[#33FFC2] bg-[rgba(61,79,61,0.7)] border border-transparent hover:border-[#33FFC2]">
               Home
             </button>
           </Link>
 
           <Link to="/about">
-            <button type="button" className="h-12 w-20 text-white hover:text-[#33FFC2] hover:bg-[rgba(61,79,61,0.7)]">
+            <button type="button" className="h-12 w-20 text-[14px] text-white hover:text-[#33FFC2] hover:bg-[rgba(61,79,61,0.7)]">
               About Us
             </button>
           </Link>
           
           <Link to="/members">
-            <button type="button" className="h-12 w-20 text-white hover:text-[#33FFC2] hover:bg-[rgba(61,79,61,0.7)]">
+            <button type="button" className="h-12 w-20 text-[14px] text-white hover:text-[#33FFC2] hover:bg-[rgba(61,79,61,0.7)]">
               Members
             </button>
           </Link>
 
           <Link to="/project">
-            <button type="button" className="h-12 w-20 text-white hover:text-[#33FFC2] hover:bg-[rgba(61,79,61,0.7)]">
+            <button type="button" className="h-12 w-20 text-[14px] text-white hover:text-[#33FFC2] hover:bg-[rgba(61,79,61,0.7)]">
               Projects
             </button>
           </Link>
 
           <Link to="/contact">
-            <button type="button" className="h-12 w-20 text-white hover:text-[#33FFC2] hover:bg-[rgba(61,79,61,0.7)]">
+            <button type="button" className="h-12 w-20 text-[14px] text-white hover:text-[#33FFC2] hover:bg-[rgba(61,79,61,0.7)]">
               Contact Us
             </button>
           </Link>
--- Jelizen/src/Pages/Homepage.jsx
+++ Jelizen/src/Pages/Homepage.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React from 'react'
+import logobig from '../assets/pics/logobig.png';
 
-function Homepage(){
-  return (    
-    <div>Homepage, try</div>
-  );
+const Homepage = () => {
+  return (
+    <section className='m-40 flex flex-col justify-center items-center'>
+      <div className = "grid gap-2">
+        <img className = "max-h-35" src = {logobig}></img>
+      </div>
+
+      <div>
+        <p className='text-[30px] text-[#33FFC2] font-semibold tracking-[15px]'>
+          Jelizen
+        </p>
+      </div>
+
+      <div>
+        <p className='text-[13px] text-white tracking-wide'>
+          expand your imagination
+        </p>
+      </div>
+    </section>
+  )
 }
 
-export default Homepage;
+export default Homepage
