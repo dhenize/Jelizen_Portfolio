d3f2fa200bbd1162e5e15f31c2583698010a45ea
unknown <dhenizelopez@gmail.com>
2025-04-05 18:49:22 +0800

    naglagay lang ng comments sa homepage

--- Jelizen/src/Pages/Homepage.jsx
+++ Jelizen/src/Pages/Homepage.jsx
@@ -3,24 +3,62 @@ import logobig from '../assets/pics/logobig.png';
 
 const Homepage = () => {
   return (
-    <section className='m-40 flex flex-col justify-center items-center'>
-      <div className = "grid gap-2">
-        <img className = "max-h-35" src = {logobig}></img>
+    <section>
+      {/*OPENING (LOGO, NAME, TAGLINE*/}
+      <div className='m-40 flex flex-col justify-center items-center'>
+        <div className = "grid gap-2">
+          <img className = "max-h-35" src = {logobig}></img>
+        </div>
+
+        <div>
+          <p className='text-[30px] text-[#33FFC2] font-semibold tracking-[15px]'>
+            Jelizen
+          </p>
+        </div>
+
+        <div>
+          <p className='text-[13px] text-white tracking-wide'>
+            expand your imagination
+          </p>
+        </div>
       </div>
 
+
+
+      {/*INTRODUCTION TEXT*/}
       <div>
-        <p className='text-[30px] text-[#33FFC2] font-semibold tracking-[15px]'>
-          Jelizen
-        </p>
+
       </div>
 
+
+
+      {/*MISSION AND VISION*/}
       <div>
-        <p className='text-[13px] text-white tracking-wide'>
-          expand your imagination
-        </p>
+
+      </div>
+
+
+
+      {/*PROFILE/MEMBERS*/}
+      <div>
+
+      </div>
+
+
+
+      {/*ABOUT US*/}
+      <div>
+
+      </div>
+
+
+
+      {/*TECHNICAL SKILLS*/}
+      <div>
+
       </div>
     </section>
-  )
+  );
 }
 
 export default Homepage
